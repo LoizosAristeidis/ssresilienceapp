0513356789ca75f2162ff033e58298fc2fdd1959
christos christodoulou <christoschristodoulou3@gmail.com>
2021-07-05 00:03:57 +0300

    created firebase authentication and database storage

--- app/src/main/java/com/example/ssresilience/MainActivity.java
+++ app/src/main/java/com/example/ssresilience/MainActivity.java
@@ -1,13 +1,33 @@
 package com.example.ssresilience;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        register = (Button) findViewById(R.id.register_screen);
+        register.setOnClickListener(this);
+
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.register_screen:
+                startActivity(new Intent(this, RegisterUserActivity.class));
+                break;
+        }
     }
 }
