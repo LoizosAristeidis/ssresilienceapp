354d18784da6b817cc4c07b589cf14bd7d679f51
christos christodoulou <christoschristodoulou3@gmail.com>
2021-07-06 21:54:29 +0300

    finally finished login and signup

--- app/src/main/java/com/example/ssresilience/MainActivity.java
+++ app/src/main/java/com/example/ssresilience/MainActivity.java
@@ -2,14 +2,23 @@ package com.example.ssresilience;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private Button register;
+    private Button register, login_btn;
+    private EditText login_email, login_password;
+    private ProgressBar progressBar;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +27,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         register = (Button) findViewById(R.id.register_screen);
         register.setOnClickListener(this);
+
+        login_btn = (Button) findViewById(R.id.login_btn);
+        login_btn.setOnClickListener(this);
+
+        login_email = (EditText) findViewById(R.id.login_email);
+        login_password = (EditText) findViewById(R.id.login_password);
+
+        progressBar = (ProgressBar) findViewById(R.id.login_progressBar);
+
+        mAuth = FirebaseAuth.getInstance();
 
     }
 
@@ -28,6 +47,43 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.register_screen:
                 startActivity(new Intent(this, RegisterUserActivity.class));
                 break;
+            case R.id.login_btn:
+                userLogin();
+                break;
         }
+    }
+
+    private void userLogin() {
+        String email = login_email.getText().toString().trim();
+        String password = login_password.getText().toString().trim();
+
+        if(email.isEmpty()) {
+            login_email.setError("Email cannot be blank");
+            login_email.requestFocus();
+            return;
+        }
+
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            login_email.setError("Please enter a valid email");
+            login_email.requestFocus();
+            return;
+        }
+
+        if(password.isEmpty()) {
+            login_password.setError("Password is required");
+            login_password.requestFocus();
+            return;
+        }
+
+        progressBar.setVisibility(View.VISIBLE);
+
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(task -> {
+            if(task.isSuccessful()) {
+                //redirect to userprofile or wherever we want to redirect.. (main page / goal page)
+                startActivity(new Intent(this, ProfileActivity.class));
+            } else {
+                Toast.makeText(MainActivity.this, "FAILED TO LOGIN! PLEASE CHECK YOUR CREDENTIALS", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
