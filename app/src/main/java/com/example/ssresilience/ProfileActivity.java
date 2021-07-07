73d8fa3ba6aaae52d550c9a7fd61f7db99665764
christos christodoulou <christoschristodoulou3@gmail.com>
2021-07-07 12:23:23 +0300

    adding hte user id in the next screen

--- app/src/main/java/com/example/ssresilience/ProfileActivity.java
+++ app/src/main/java/com/example/ssresilience/ProfileActivity.java
@@ -1,34 +1,40 @@
 package com.example.ssresilience;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
 import android.net.Uri;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class ProfileActivity extends AppCompatActivity {
 
+    private Button profile_logout;
+    private TextView profile_username;
+
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
-        //test this to get the user's id
+        profile_logout = (Button) findViewById(R.id.profile_logout);
+        profile_username = (TextView) findViewById(R.id.profile_username);
 
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         if (user != null) {
-            // Name, email address, and profile photo Url
-            String name = user.getDisplayName();
-            String email = user.getEmail();
-            Uri photoUrl = user.getPhotoUrl();
-
-            // Check if user's email is verified
-            boolean emailVerified = user.isEmailVerified();
-
-            // The user's ID, unique to the Firebase project. Do NOT use this value to
-            // authenticate with your backend server, if you have one. Use
-            // FirebaseUser.getIdToken() instead.
             String uid = user.getUid();
+            profile_username.setText("Hello" + uid);
+
+            FirebaseDatabase database = FirebaseDatabase.getInstance();
+            DatabaseReference usersRef = database.getReference("Users");
+            usersRef.orderByChild(uid);
         }
     }
 }
