b2880a5b381632fc1e00d3df22ccd843dd000194
LoizosAristeidis <loizosapollwn@gmail.com>
2021-07-11 04:20:45 +0300

    startup-screen-completion

--- app/src/main/java/com/example/ssresilience/HomePageActivity.java
+++ app/src/main/java/com/example/ssresilience/HomePageActivity.java
@@ -1,7 +1,11 @@
 package com.example.ssresilience;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,8 +15,14 @@ import androidx.fragment.app.FragmentTransaction;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+// Purple Color: #3700b3
+
 public class HomePageActivity extends AppCompatActivity {
     BottomNavigationView bottomNavigation;
+    private Button info_goals, info_measure_monitor, info_progress_rewards, info_share, info_reflect;
+    private TextView info_placeholder;
+    private boolean tmp = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +30,64 @@ public class HomePageActivity extends AppCompatActivity {
         bottomNavigation = findViewById(R.id.bottom_navigation);
         bottomNavigation.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
 //        openFragment(GoalsFragment.newInstance("", ""));
+
+        info_goals = findViewById(R.id.info_goals);
+        info_measure_monitor = findViewById(R.id.info_measure_monitor);
+        info_progress_rewards = findViewById(R.id.info_progress_rewards);
+        info_share = findViewById(R.id.info_share);
+        info_reflect = findViewById(R.id.info_reflect);
+        info_placeholder = findViewById(R.id.info_placeholder);
+
+        info_goals.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                if (info_placeholder.getText().toString().equals("")) {
+                    info_placeholder.setText("You can set a different goal each day");
+                }
+
+            }
+        });
+
+        info_measure_monitor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                if (info_placeholder.getText().toString().equals("")) {
+                    info_placeholder.setText("Use your device's sensors or the stress test to self-report your emotions");
+                }
+            }
+        });
+
+        info_progress_rewards.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                if (info_placeholder.getText().toString().equals("")) {
+                    info_placeholder.setText("These are based on your progress within the app");
+                }
+            }
+        });
+
+        info_share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                if (info_placeholder.getText().toString().equals("")) {
+                    info_placeholder.setText("Compete or Collaborate with other users");
+                }
+            }
+        });
+
+        info_reflect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                if (info_placeholder.getText().toString().equals("")) {
+                    info_placeholder.setText("Be your own judge at the end of each day");
+                }
+            }
+        });
     }
 
     public void openFragment(Fragment fragment) {
