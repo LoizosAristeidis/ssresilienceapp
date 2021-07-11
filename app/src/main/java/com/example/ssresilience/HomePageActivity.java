72b42633ad0d8026be5d3491eeab99d212b13e8a
LoizosAristeidis <loizosapollwn@gmail.com>
2021-07-11 12:46:30 +0300

    startup-screen-completion

--- app/src/main/java/com/example/ssresilience/HomePageActivity.java
+++ app/src/main/java/com/example/ssresilience/HomePageActivity.java
@@ -1,5 +1,6 @@
 package com.example.ssresilience;
 
+import android.annotation.SuppressLint;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -15,13 +16,15 @@ import androidx.fragment.app.FragmentTransaction;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import static com.example.ssresilience.R.color.buttoncolor_text;
+
 // Purple Color: #3700b3
 
+@SuppressWarnings("deprecation")
 public class HomePageActivity extends AppCompatActivity {
     BottomNavigationView bottomNavigation;
     private Button info_goals, info_measure_monitor, info_progress_rewards, info_share, info_reflect;
     private TextView info_placeholder;
-    private boolean tmp = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,22 +47,73 @@ public class HomePageActivity extends AppCompatActivity {
         info_placeholder = findViewById(R.id.info_placeholder);
     }
 
+    @SuppressLint({"UseCompatLoadingForDrawables", "ResourceAsColor", "UseCompatLoadingForColorStateLists", "NonConstantResourceId", "SetTextI18n"})
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.info_goals:
                     info_placeholder.setText("You can set a different goal each day");
+                    info_goals.setBackgroundDrawable(getResources().getDrawable(R.drawable.buttoncolor));
+                    info_goals.setTextColor(getResources().getColorStateList(R.color.buttoncolor_text));
+                    info_measure_monitor.setBackgroundDrawable(getResources().getDrawable(R.drawable.buttoncolor_reset));
+                    info_measure_monitor.setTextColor(getResources().getColorStateList(R.color.buttoncolor_text_reset));
+                    info_progress_rewards.setBackgroundDrawable(getResources().getDrawable(R.drawable.buttoncolor_reset));
+                    info_progress_rewards.setTextColor(getResources().getColorStateList(R.color.buttoncolor_text_reset));
+                    info_share.setBackgroundDrawable(getResources().getDrawable(R.drawable.buttoncolor_reset));
+                    info_share.setTextColor(getResources().getColorStateList(R.color.buttoncolor_text_reset));
+                    info_reflect.setBackgroundDrawable(getResources().getDrawable(R.drawable.buttoncolor_reset));
+                    info_reflect.setTextColor(getResources().getColorStateList(R.color.buttoncolor_text_reset));
                 break;
             case R.id.info_measure_monitor:
                     info_placeholder.setText("Use your device's sensors or the stress test to self-report your emotions");
+                    info_goals.setBackgroundDrawable(getResources().getDrawable(R.drawable.buttoncolor_reset));
+                    info_goals.setTextColor(getResources().getColorStateList(R.color.buttoncolor_text_reset));
+                    info_measure_monitor.setBackgroundDrawable(getResources().getDrawable(R.drawable.buttoncolor));
+                    info_measure_monitor.setTextColor(getResources().getColorStateList(R.color.buttoncolor_text));
+                    info_progress_rewards.setBackgroundDrawable(getResources().getDrawable(R.drawable.buttoncolor_reset));
+                    info_progress_rewards.setTextColor(getResources().getColorStateList(R.color.buttoncolor_text_reset));
+                    info_share.setBackgroundDrawable(getResources().getDrawable(R.drawable.buttoncolor_reset));
+                    info_share.setTextColor(getResources().getColorStateList(R.color.buttoncolor_text_reset));
+                    info_reflect.setBackgroundDrawable(getResources().getDrawable(R.drawable.buttoncolor_reset));
+                    info_reflect.setTextColor(getResources().getColorStateList(R.color.buttoncolor_text_reset));
                 break;
             case R.id.info_progress_rewards:
                     info_placeholder.setText("These are based on your progress within the app");
+                    info_goals.setBackgroundDrawable(getResources().getDrawable(R.drawable.buttoncolor_reset));
+                    info_goals.setTextColor(getResources().getColorStateList(R.color.buttoncolor_text_reset));
+                    info_measure_monitor.setBackgroundDrawable(getResources().getDrawable(R.drawable.buttoncolor_reset));
+                    info_measure_monitor.setTextColor(getResources().getColorStateList(R.color.buttoncolor_text_reset));
+                    info_progress_rewards.setBackgroundDrawable(getResources().getDrawable(R.drawable.buttoncolor));
+                    info_progress_rewards.setTextColor(getResources().getColorStateList(R.color.buttoncolor_text));
+                    info_share.setBackgroundDrawable(getResources().getDrawable(R.drawable.buttoncolor_reset));
+                    info_share.setTextColor(getResources().getColorStateList(R.color.buttoncolor_text_reset));
+                    info_reflect.setBackgroundDrawable(getResources().getDrawable(R.drawable.buttoncolor_reset));
+                    info_reflect.setTextColor(getResources().getColorStateList(R.color.buttoncolor_text_reset));
                 break;
             case R.id.info_share:
                     info_placeholder.setText("Compete or Collaborate with other users");
+                    info_goals.setBackgroundDrawable(getResources().getDrawable(R.drawable.buttoncolor_reset));
+                    info_goals.setTextColor(getResources().getColorStateList(R.color.buttoncolor_text_reset));
+                    info_measure_monitor.setBackgroundDrawable(getResources().getDrawable(R.drawable.buttoncolor_reset));
+                    info_measure_monitor.setTextColor(getResources().getColorStateList(R.color.buttoncolor_text_reset));
+                    info_progress_rewards.setBackgroundDrawable(getResources().getDrawable(R.drawable.buttoncolor_reset));
+                    info_progress_rewards.setTextColor(getResources().getColorStateList(R.color.buttoncolor_text_reset));
+                    info_share.setBackgroundDrawable(getResources().getDrawable(R.drawable.buttoncolor));
+                    info_share.setTextColor(getResources().getColorStateList(R.color.buttoncolor_text));
+                    info_reflect.setBackgroundDrawable(getResources().getDrawable(R.drawable.buttoncolor_reset));
+                    info_reflect.setTextColor(getResources().getColorStateList(R.color.buttoncolor_text_reset));
                 break;
             case R.id.info_reflect:
                     info_placeholder.setText("Be your own judge at the end of each day");
+                    info_goals.setBackgroundDrawable(getResources().getDrawable(R.drawable.buttoncolor_reset));
+                    info_goals.setTextColor(getResources().getColorStateList(R.color.buttoncolor_text_reset));
+                    info_measure_monitor.setBackgroundDrawable(getResources().getDrawable(R.drawable.buttoncolor_reset));
+                    info_measure_monitor.setTextColor(getResources().getColorStateList(R.color.buttoncolor_text_reset));
+                    info_progress_rewards.setBackgroundDrawable(getResources().getDrawable(R.drawable.buttoncolor_reset));
+                    info_progress_rewards.setTextColor(getResources().getColorStateList(R.color.buttoncolor_text_reset));
+                    info_share.setBackgroundDrawable(getResources().getDrawable(R.drawable.buttoncolor_reset));
+                    info_share.setTextColor(getResources().getColorStateList(R.color.buttoncolor_text_reset));
+                    info_reflect.setBackgroundDrawable(getResources().getDrawable(R.drawable.buttoncolor));
+                    info_reflect.setTextColor(getResources().getColorStateList(R.color.buttoncolor_text));
                 break;
             default:
                 break;
