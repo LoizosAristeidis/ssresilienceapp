b6d3b6a9535110ec0a6649d636bb1aa4b6741bf5
LoizosAristeidis <loizosapollwn@gmail.com>
2021-07-11 16:00:31 +0300

    startup-screen-completion

--- app/src/main/java/com/example/ssresilience/RegisterUserActivity.java
+++ app/src/main/java/com/example/ssresilience/RegisterUserActivity.java
@@ -1,9 +1,12 @@
 package com.example.ssresilience;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
 import android.util.Patterns;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
@@ -53,6 +56,15 @@ public class RegisterUserActivity extends AppCompatActivity implements View.OnCl
 
         // Hide the Action Bar for this Activity
         getSupportActionBar().hide();
+    }
+
+    // Hide keyboard when user taps outside the Edittext box
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        InputMethodManager imm = (InputMethodManager)getSystemService(Context.
+                INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+        return true;
     }
 
     @SuppressLint("NonConstantResourceId")
